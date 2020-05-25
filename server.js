eb9c128790eca01dbb69db29934133d4321d0555
Shahin1991 <31946584+Shahin1991@users.noreply.github.com>
2020-05-25 14:30:30 +0400

    production config

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
-
+const path = require('path')
 const app = express();
 
 //connect DB
@@ -10,7 +10,7 @@ connectDB();
 //Init middleware
 app.use(express.json({extended:false}));  
 
-app.get('/',(req,res)=>res.send('API Running'));
+// app.get('/',(req,res)=>res.send('API Running'));
 
 //Define routes
 app.use('/api/users',require('./routes/api/users'))
@@ -18,6 +18,14 @@ app.use('/api/posts',require('./routes/api/posts'))
 app.use('/api/profile',require('./routes/api/profile'))
 app.use('/api/auth',require('./routes/api/auth'))
 
+//Serve static assets in production
+if(process.env.NODE_ENV==='production'){
+    //set static folder
+    app.use(express.static('client/build'));
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    })
+}
 
 const PORT = process.env.PORT||5000;
 
